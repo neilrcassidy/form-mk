3efb2291272b13399dcb0db50b7c49829ca33447
neilrcassidy <neilrobertcassidy@gmail.com>
2026-02-05 12:13:13 +0100

    added hosting

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -1,5 +1,7 @@
 import bgImage from "./bgImage.jpg"
+import bgImage2 from "./bgImage2.jpg"
 
 export {
-  bgImage
+  bgImage,
+  bgImage2
 }
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,10 +3,10 @@ import { bgImage } from '../assets/img'
 const Background = () => {
   return (
     <>
-    <div className='w-[100vw] h-[100vh] absolute'>
-      <img src={bgImage} className='absolute w-full h-full object-cover object-bottom'></img>
-      <div className='absolute w-full h-full bg-gray-800 opacity-90'></div>
-    </div>
+      <div className='w-[100vw] h-[100vh] absolute'>
+        <img src={bgImage} className='absolute w-full h-full object-cover object-bottom'></img>
+        <div className='absolute w-full h-full bg-gray-800 opacity-95'></div>
+      </div>
     </>
   )
 }
